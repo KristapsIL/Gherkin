47e7fdf43985f77aeb76142a4c3d7b5545338b32
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-20 11:16:53 +0200

    a

--- src/Feature.js
+++ src/Feature.js
@@ -5,7 +5,7 @@ function Feature(){
             <div>
                 <h2>Kas un kāpēc ir funkcionālā iezīme?</h2>
                 <p>Funkcionālā iezīme ir ieviesta funkcionalitāte
-                mūsu sistēma,  lai atbalstītu spēju . Funkcionālā iezīme sastāv vismaz no funkcionālā iezīmes
+                mūsu sistēma,  lai atbalstītu spēju . Funkcionālā iezīme sastāv vismaz no funkcionālās iezīmes
                 Nosaukuma un scenārija.</p>
                 <ul>
 
