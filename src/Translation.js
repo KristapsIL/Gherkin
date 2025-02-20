bac59cdf49dfcb23de3c91d482d638d3a0026bcb
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-20 09:04:07 +0200

    first

---
+++ src/Translation.js
@@ -0,0 +1,51 @@
+function Translation(){
+return(
+    <>
+        <tr>
+            <td>Feature</td>
+            <td>Funkcionala iezime</td>
+        </tr>
+        <tr>
+            <td>background</td>
+            <td>konteksts</td>
+        </tr>
+        <tr>
+            <td>scenario</td>
+            <td>scenarijs</td>
+        </tr>
+        <tr>
+            <td>scenario outline</td>
+            <td>scenarijs pec parauga</td>
+        </tr>
+        <tr>
+            <td>exaples</td>
+            <td>piemeri</td>
+        </tr>
+        <tr>
+            <td>given</td>
+            <td>kad</td>
+        </tr>
+        <tr>
+            <td>when</td>
+            <td>ja</td>
+        </tr>
+        <tr>
+            <td>then</td>
+            <td>tad</td>
+        </tr>
+        <tr>
+            <td>and</td>
+            <td>un</td>
+        </tr>
+        <tr>
+            <td>but</td>
+            <td>bet</td>
+        </tr>
+        <tr>
+            <td>or</td>
+            <td>vai</td>
+        </tr>
+    </>
+    )   
+}
+export default Translation
---
+++ src/Translations.js
@@ -0,0 +1,19 @@
+import Translation from "./Translation";
+function Translations(){
+    return (
+        <>
+            <table id="table">
+                <thead>
+                    <tr>
+                        <th>Angļu valodā</th>
+                        <th>Latviešu valodā</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Translation/>
+                </tbody>
+            </table>
+        </>
+    )
+}
+export default Translations;
